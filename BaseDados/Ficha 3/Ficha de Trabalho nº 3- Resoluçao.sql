7c90e6cb5e43b3e6dc796d9e2fcf49e2f074f71c
Nuno Miguel <67641470+Nuno777@users.noreply.github.com>
2022-06-23 21:23:07 +0100

    Delete BaseDados directory

--- BaseDados/Ficha 3/Ficha de Trabalho nº 3- Resoluçao.sql
+++
@@ -1,6 +0,0 @@
-/* -1- */select MIN(CodigoDepartamento) as "Menor Código" from Empregados;
-/* -2- */select nome from empregados where CodigoDepartamento = 20;
-/* -3- */Select nome,salario from empregados where salario=(select MIN(salario)from empregados); 
-/* -4- */update Empregados set salario=salario*0.10 where CodigoDepartamento = 10;
-/* -5- */select NEmpregado as "Empregado", nome as "Nome" from empregados where codigodepartamento = (select codigodepartamento from departamentos where Localizacao = "Lisboa" order by nome);
-/* -6- */select Nome, Departamento, Categoria from empregados inner join departamentos on empregados.codigodepartamento = departamentos.codigodepartamento order by categoria DESC, Nome ASC;
